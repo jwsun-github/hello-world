7cb56d4af52d082fbd5b3654f58cbc8cf06a9cd8
jason sun <jwsun3@gmail.com>
2017-03-16 18:51:24 -0400

    add hello-world python program

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+x = int(input("please type in a number:")
+y = int(input("please type in another number:")
+
+if x==y :
+	print("Hello World! The numbers you enter are equal!")
+else :
+	print("too bad! The numbers you enter are NOT equal!")
