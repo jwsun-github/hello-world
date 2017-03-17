fbaf9c7ca700bf86d365ff15f850d7e0ec07de0a
jason sun <jwsun3@gmail.com>
2017-03-16 21:15:08 -0400

    fix parenthesis error

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,5 @@
-x = int(input("please type in a number:")
-y = int(input("please type in another number:")
+x = int(input("please type in a number:"))
+y = int(input("please type in another number:"))
 
 if x==y :
 	print("Hello World! The numbers you enter are equal!")
